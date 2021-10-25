33e92230586842cfdc2f9c2c467d359b4f217cde
rajarshih31 <rajarshih@gmail.com>
2021-10-26 00:34:44 +1100

    fix: added docker image and services in build and deploy stage

--- build_and_push_docker_image.sh
+++ build_and_push_docker_image.sh
@@ -3,18 +3,18 @@
 # 'DOCKER_PASSWORD','DOCKER_USERNAME' & 'REPO_NAME'need to be set set as environment variables of the pipeline.
 
 # First build the image
-docker build -t anzbank-test-app .
-echo "Dcker build success!"
+sudo docker build -t anzbank-test-app .
+echo "Docker build success!"
 
 #Login to Dockerhub. 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 echo "DockerHub login success!"
 
 #Tag the image before pushing it to Docker Hub.
 #sudo docker tag anzbank-test-app rajarshih6643/anzbank-test-repo:anzbank-test-app
-docker tag anzbank-test-app $DOCKER_USERNAME/$REPO_NAME:anzbank-test-app
+sudo docker tag anzbank-test-app $DOCKER_USERNAME/$REPO_NAME:anzbank-test-app
 
 #Now, push the image to my Docker hub repository.
 #sudo docker push rajarshih6643/anzbank-test-repo:anzbank-test-app
-docker push $DOCKER_USERNAME/$REPO_NAME:anzbank-test-app
+sudo docker push $DOCKER_USERNAME/$REPO_NAME:anzbank-test-app
 echo "Pushed the image to dockerhub successfully"
