a290de72d164471cf3e2a0cdb360611e90815d32
rajarshih31 <rajarshih@gmail.com>
2021-10-25 23:41:49 +1100

    fix: Fixed the application port in integration tests

--- test/test-application.js
+++ test/test-application.js
@@ -5,14 +5,14 @@ const app = require('../application')
   describe("GET /version API Tests", () => {
 	
 	    it('/version API response status - should be 200', function(done) {
-			request('http://localhost:8081/version' , function(error, response, body) {
+			request('http://localhost:8080/version' , function(error, response, body) {
 				expect(response.statusCode).to.equal(200);
 				done();
 			});
 		});
 
 		it('/version API version check', function(done) {
-			request('http://localhost:8081/version' , function(error, response, body) {
+			request('http://localhost:8080/version' , function(error, response, body) {
 				var jsonObj = JSON.parse(body);
 				expect(jsonObj.version).to.equal('1.0.0');							
 				done();
@@ -20,7 +20,7 @@ const app = require('../application')
 		});
 		
 		it('/version API description check', function(done) {
-			request('http://localhost:8081/version' , function(error, response, body) {				
+			request('http://localhost:8080/version' , function(error, response, body) {				
 				var jsonObj = JSON.parse(body);
 				expect(jsonObj.description).to.equal('pre-interview technical test');
 				done();
